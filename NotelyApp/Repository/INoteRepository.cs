c3a526663851b5ccd2b36a8257f0f37b038d031f
noaha27 <noahddk@gmail.com>
2020-10-10 12:22:00 -0700

    first commit

---
+++ NotelyApp/Repository/INoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using NotelyApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace NotelyApp.Repositories
+{
+    public interface INoteRepository
+    {
+        void DeleteNote(NoteModel noteModel);
+        NoteModel FindNoteById(Guid id);
+        IEnumerable<NoteModel> GetAllNotes();
+        void SaveNote(NoteModel noteModel);
+    }
+}
---
+++ NotelyApp/Repository/NoteRepository.cs
@@ -0,0 +1,42 @@
+﻿using NotelyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace NotelyApp.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly List<NoteModel> _notes;
+
+        public NoteRepository()
+        {
+            _notes = new List<NoteModel>();
+        }
+
+        public NoteModel FindNoteById(Guid id)
+        {
+            var note = _notes.Find(match: n => n.Id == id);
+
+            return note;
+        }
+
+        public IEnumerable<NoteModel> GetAllNotes()
+        {
+            return _notes;
+        }
+
+        public void SaveNote(NoteModel noteModel)
+        {
+            _notes.Add(noteModel);
+        }
+
+        public void DeleteNote(NoteModel noteModel)
+        {
+            _notes.Remove(noteModel);
+        }
+    }
+}
+
